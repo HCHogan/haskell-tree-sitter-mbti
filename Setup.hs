0e990460bfdb2962561d959a7eb75c43bec3d988
Hank Hogan <ysh2291939848@outlook.com>
2025-07-14 19:00:36 +0800

    fix: robust setup script

--- Setup.hs
+++ Setup.hs
@@ -1,18 +1,53 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Data.Maybe
+import Control.Monad (void, when)
 import Distribution.Simple
-import Distribution.Simple.Setup
-import Distribution.PackageDescription
-import System.Process
-import System.Directory
-import Control.Monad (when)
+import Distribution.Simple.Setup (
+  BuildFlags (..),
+  Flag,
+  flagToMaybe,
+ )
+import Distribution.Simple.Utils (info)
+import Distribution.Types.HookedBuildInfo
+import Distribution.Verbosity (Verbosity, normal)
+import System.Directory (
+  canonicalizePath,
+  doesDirectoryExist,
+  getCurrentDirectory,
+ )
+import System.FilePath ((</>))
+import System.Process (
+  createProcess,
+  cwd,
+  proc,
+  waitForProcess,
+ )
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks
-  { preBuild = \_ _ -> do
-      putStrLn "Running tree-sitter generate for tree-sitter-mbti..."
-      let dir = "vendor/tree-sitter-mbti"
-      exists <- doesDirectoryExist dir
-      when exists $ do
-        callCommand $ "cd " ++ dir ++ " && tree-sitter generate --abi 14"
-      return emptyHookedBuildInfo
-  }
+main =
+  defaultMainWithHooks
+    simpleUserHooks
+      { preBuild = runTreeSitterGenerate
+      }
 
+runTreeSitterGenerate :: Args -> BuildFlags -> IO HookedBuildInfo
+runTreeSitterGenerate _ BuildFlags{..} =
+  let logV :: Flag Verbosity -> String -> IO ()
+      logV v = info (fromMaybe normal (flagToMaybe v))
+   in do
+        pwd <- getCurrentDirectory >>= canonicalizePath
+        let tsDir = pwd </> "vendor" </> "tree-sitter-mbti"
+
+        exists <- doesDirectoryExist tsDir
+
+        when exists $
+          do
+            (_, _, _, ph) <-
+              createProcess
+                (proc "tree-sitter" ["generate", "--abi", "14"])
+                  { cwd = Just tsDir
+                  }
+            void (waitForProcess ph)
+
+        pure emptyHookedBuildInfo
